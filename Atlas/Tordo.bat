d5e3f430b268ce7aa3cb92c23e2e7d822ef9f863
Darrman <30606600+Darrman@users.noreply.github.com>
2021-03-04 20:18:56 +0000

    Cleaned up all overflow
    Overflowing class names were put into free space, extra castles were merged, and various other missing messages were dealt with. Combat forecast and fixing the stat screen alignment is next.

--- Atlas/Tordo.bat
+++ Atlas/Tordo.bat
@@ -1,4 +1,6 @@
 ::Menu text
 Atlas -d "log\menus.log" "ROM\Tordo.sfc" "script\menus.txt"
+::ASM adjustments, manually enable this when necessary
+::Atlas -d "log\ASM.log" "ROM\Tordo.sfc" "script\asm.txt"
 
 pause
