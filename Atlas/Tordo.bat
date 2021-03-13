89253c3c54b190a27454d70352c6141943146e61
Darrman <30606600+Darrman@users.noreply.github.com>
2021-03-13 04:03:13 +0000

    Dialogue names inserted
    Now the fortune teller and promotion screens are somewhat usable. Now to fix up the shop dialogue since the pointers got eaten. Then expand the chapter names because their vanilla space is tiny. Then play Gen 2 and sweep for bugs!

--- Atlas/Tordo.bat
+++ Atlas/Tordo.bat
@@ -1,6 +1,8 @@
 ::Menu text
 Atlas -d "log\menus.log" "ROM\Tordo.sfc" "script\menus.txt"
 ::ASM adjustments, manually enable this when necessary
-::Atlas -d "log\ASM.log" "ROM\Tordo.sfc" "script\asm.txt"
+Atlas -d "log\ASM.log" "ROM\Tordo.sfc" "script\asm.txt"
+perl ..\abcde.pl -cm abcde::Atlas "ROM\Tordo.sfc" "script\system.txt"
+
 
 pause
